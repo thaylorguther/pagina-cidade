6e53e355c51d44e3e6a6dcf76e5317c6933b6896
Thaylor Guther <159261485+thaylorguther@users.noreply.github.com>
2024-07-31 09:41:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let lamp=  document.getElementById("imagem")
+let legendas= document.getElementById("legenda")
+function imagem1(){
+
+    lamp.src = 'Hollywood.png'
+    legendas.innerHTML= 'letreiro Hollywood'
+    
+}
+function imagem2(){
+
+    lamp. src = 'crypto.png'
+    legendas.innerHTML= 'Crypto.com Arena'
+    
+}
+
+function imagem3(){
+
+    lamp.src = '2paccalçadadafama.png'
+    legendas.innerHTML= 'Calçada da fama'
+}
+function imagem4(){
+
+    lamp. src = 'LAKERS.png'
+    legendas.innerHTML= 'Time de basquete'
+}
